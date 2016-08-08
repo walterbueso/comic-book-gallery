1181a26b54a8378b4d49b57e2c19d1029fdc3800
Walter Bueso <walter.bueso@dos.myflorida.com>
2016-08-08 15:23:56 -0400

    updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -7,5 +7,29 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        //to add a title for each comic book
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " " + IssueNumber;
+            }
+        }
+
+        //To get the cover image with name convention series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
