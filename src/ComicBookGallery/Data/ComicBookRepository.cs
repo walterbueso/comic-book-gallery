bfbbd6d61a25ef1eae6e8b162fd8e793e61e3d83
Walter Bueso <walter.bueso@dos.myflorida.com>
2016-09-10 21:15:11 -0400

    Added comic books list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -14,6 +14,7 @@ namespace ComicBookGallery.Data
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
+                Id = 1,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                 {
@@ -29,6 +30,7 @@ namespace ComicBookGallery.Data
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
+                Id = 2,
                 DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
                 Artists = new Artist[]
                 {
@@ -44,6 +46,7 @@ namespace ComicBookGallery.Data
             {
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
+                Id = 3,
                 DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
                 Artists = new Artist[]
                 {
@@ -72,5 +75,12 @@ namespace ComicBookGallery.Data
 
             return comicBookToReturn;
         }
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
+
     }
 }
